2ee55330def26a695f671dffd454df1088fcaa49
Mitchell <Hmitchell.hall@gmail.com>
2021-08-13 21:02:44 -0500

    commit tree b4d482ba3ca9628d340bfab56a9ac40693104bdc

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+const { objectContaining } = require("expect")
+
 // Code your solution here
+function findMatching(drivers, name){
+    const found = drivers.filter(element => element.toLowerCase() === name.toLowerCase()) 
+    //return drivers.filter(element => element === name)
+    //debugger;
+    return found    
+}
+function fuzzyMatch(drivers, str){
+   return drivers.filter(driver => driver.toLowerCase().indexOf(str.toLowerCase()) === 0) 
+}
+
+
+//   drivers.filter(function(driver) {
+    // code here
+// })
+
+
+ function matchName(drivers, str){
+    return drivers.filter(obj => obj.name === str)
+  
+ }
